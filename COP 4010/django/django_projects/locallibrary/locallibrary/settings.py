8d6177e5006a4e12ceb88698eb88d5c66dc81b0f
SGremillion <102303563+SGremillion@users.noreply.github.com>
2022-10-17 16:55:04 -0400

    Add files via upload

--- COP 4010/django/django_projects/locallibrary/locallibrary/settings.py
+++ COP 4010/django/django_projects/locallibrary/locallibrary/settings.py
@@ -20,10 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&(8#-9s&e_%_n*yebs_27$w%j=z1e3zr209d(ltq$2sw5@*t9!'
+# SECRET_KEY = 'django-insecure-&(8#-9s&e_%_n*yebs_27$w%j=z1e3zr209d(ltq$2sw5@*t9!'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
