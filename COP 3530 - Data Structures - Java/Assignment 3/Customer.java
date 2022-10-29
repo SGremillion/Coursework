2d18850014a1a92bf9c541bf231000c5bcb18bbd
SGremillion <102303563+SGremillion@users.noreply.github.com>
2022-10-29 16:11:05 -0400

    Added files

---
+++ COP 3530 - Data Structures - Java/Assignment 3/Customer.java
@@ -0,0 +1,11 @@
+class Customer {
+	// variables
+	String name, address, phone;
+
+	//constructor
+	Customer(String name, String address, String phone) {
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+	}
+}
